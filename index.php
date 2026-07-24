b8b58eeef7821300d0180b885d6ae05e6399e4c6
Harsh Patel <harsh.patel081004@gmail.com>
2026-07-24 17:32:49 +0530

    fix: update SCRIPT_NAME in root index.php to prevent route mismatch on api/login

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 chdir('/kunden/homepages/32/d853845507/htdocs/navsa-api/public');
 
 $_SERVER['SCRIPT_FILENAME'] = '/kunden/homepages/32/d853845507/htdocs/navsa-api/public/index.php';
-$_SERVER['SCRIPT_NAME'] = '/api/index.php';
-$_SERVER['PHP_SELF'] = '/api/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
 
 require '/kunden/homepages/32/d853845507/htdocs/navsa-api/public/index.php';
